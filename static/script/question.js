6bc96d0a9bb06c6a0155060a93fa16feab283f34
dhruvi1611 <122768189+dhruvi1611@users.noreply.github.com>
2023-02-21 00:53:16 +0530

    Add files via upload

---
+++ static/script/question.js
@@ -0,0 +1,237 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "what is js fullform?",
+    answer: "javascript",
+    options: [
+      "html",
+      "css",
+      "javascript",
+      "python"
+    ]
+  },
+  {
+    numb: 7,
+    question: "the people who write program call?",
+    answer: "programmer",
+    options: [
+      "cryptographers",
+      "manufactures",
+      "professors",
+      "programmer"
+    ]
+  },
+  {
+    numb: 8,
+    question: "which is programming langauge?",
+    answer: "c",
+    options: [
+      "c",
+      "banana",
+      "matelob",
+      "hubbaba"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following element is responsible for making the line break in HTML?",
+    answer: "br",
+    options: [
+      "br",
+      "bold",
+      "italic",
+      "p"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The CSS property used to control the elements font-size is ?",
+    answer: "font-size",
+    options: [
+      "font-size",
+      "size",
+      "text-size",
+      "text-style"
+    ]
+  },
+  {
+    numb: 11,
+    question: "In the virtual world, WWW stands for _____________?",
+    answer: "world wide web",
+    options: [
+      "world wide area",
+      "world white window",
+      "world wide web",
+      "world wide wirehouse"
+    ]
+  },
+  {
+    numb: 12,
+    question: "What is also known as a portable computer?",
+    answer: "laptop",
+    options: [
+      "printer",
+      "mouse",
+      "cpu",
+      "laptop"
+    ]
+  },
+  {
+    numb: 13,
+    question: "how many coding langauge?",
+    answer: "more than three",
+    options: [
+      "one",
+      "two",
+      "three",
+      "more than three"
+    ]
+  },
+  {
+    numb: 14,
+    question: "What symbol is used in python to assign values to a variable?",
+    answer: "=",
+    options: [
+      "*",
+      "=",
+      "+",
+      "+="
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which is a data type that can only be true or false?",
+    answer: "boolean",
+    options: [
+      "boolean",
+      "int",
+      "float",
+      "decimal"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Which statement will check to see if a is equal to b?",
+    answer: "if a == b:",
+    options: [
+      "if a == b",
+      "if a=b",
+      "if a == b:",
+      "if a=b:"
+    ]
+  },
+  {
+    numb: 17,
+    question: "What is needed at the start of every function declaration?",
+    answer: "def",
+    options: [
+      "def",
+      "func",
+      "dec",
+      "define"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Coding is performed at?",
+    answer: "Both A and B",
+    options: [
+      "front-end",
+      "back end",
+      "Both A and B",
+      "none of this"
+    ]
+  },
+  {
+    numb: 19,
+    question: "C++ is _______ type of programming language?",
+    answer: "Object-Oriented",
+    options: [
+      "function",
+      "Object-Oriented",
+      "external",
+      "none"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Which is the father of computer?",
+    answer: "Charles Babbage",
+    options: [
+      "Thomas Alva Edison",
+      "Michael Faraday",
+      "Tim Berners-Lee",
+      "Charles Babbage"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ static/script/word.js
@@ -0,0 +1,175 @@
+const wordList = [
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "youtube",
+        hint: "app which is use for upload videos and make money"
+    },
+    
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "amazon",
+        hint: "online shopping site"
+    },
+    {
+        word: "css",
+        hint: "programming language for style"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "aryabhatt",
+        hint: "scientist which invented 0"
+    },
+    {
+        word: "bugs",
+        hint: "related to programming error"
+    },
+    {
+        word: "bahubali",
+        hint: "epic movies of prabhas"
+    },
+    {
+        word: "gif",
+        hint: "a file format for image"
+    },
+    {
+        word: "mental",
+        hint: "related to the mind damage"
+    },
+    {
+        word: "whatsupp",
+        hint: "represent of group chat and waatching story"
+    },
+    {
+        word: "money",
+        hint: "thing which make  you rich"
+    },
+    {
+        word: "criket",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "related to an indoor game"
+    },
+    {
+        word: "snapchat",
+        hint: "a social media app"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "cpu",
+        hint: "computer component which store data and process"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "jaipur",
+        hint: "famous city call'pink city'"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "surat",
+        hint: "smart city of gujarat"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "dance",
+        hint: "related to hobies"
+    },
+    {
+        word: "tesla",
+        hint: "compnay name which is plan for driverless car"
+    },
+    {
+        word: "sun",
+        hint: "planet of our solar system"
+    },
+    
+    {
+        word: "email",
+        hint: "related to exchanging message,file and resume "
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    
+    {
+        word: "keyboard",
+        hint: "related to computer or system for type"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act of find something"
+    },
+    
+    {
+        word: "china",
+        hint: "a country name which have largest population"
+    },
+    {
+        word: "kgf",
+        hint: "highest box office collection in india 2022"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "tiger",
+        hint: "national aniaml"
+    },
+    {
+        word: "ganga",
+        hint: "largest river in india"
+    },
+    
+]
